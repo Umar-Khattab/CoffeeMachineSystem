a55d11c514f3881fd9af044c87e0e713f4e66363
Omar Khattab <omar.khattab7@icloud.com>
2024-04-18 20:49:39 +0200

    Implemenet Login Form

--- GUI/LoginForm.Designer.cs
+++ GUI/LoginForm.Designer.cs
@@ -28,18 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            idTxtBx = new Krypton.Toolkit.KryptonTextBox();
+            passTxtbx = new Krypton.Toolkit.KryptonTextBox();
+            IDLabel = new Label();
+            passLabel = new Label();
+            LoginBtn = new Krypton.Toolkit.KryptonButton();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
+            // 
+            // idTxtBx
+            // 
+            idTxtBx.CornerRoundingRadius = 15F;
+            idTxtBx.Location = new Point(366, 86);
+            idTxtBx.Name = "idTxtBx";
+            idTxtBx.PaletteMode = Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            idTxtBx.Size = new Size(255, 41);
+            idTxtBx.StateCommon.Back.Color1 = Color.FromArgb(255, 237, 217);
+            idTxtBx.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            idTxtBx.StateCommon.Border.Rounding = 15F;
+            idTxtBx.TabIndex = 0;
+            // 
+            // passTxtbx
+            // 
+            passTxtbx.CornerRoundingRadius = 15F;
+            passTxtbx.Location = new Point(366, 159);
+            passTxtbx.Name = "passTxtbx";
+            passTxtbx.PaletteMode = Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            passTxtbx.Size = new Size(255, 41);
+            passTxtbx.StateCommon.Back.Color1 = Color.FromArgb(255, 237, 217);
+            passTxtbx.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            passTxtbx.StateCommon.Border.Rounding = 15F;
+            passTxtbx.TabIndex = 1;
+            // 
+            // IDLabel
+            // 
+            IDLabel.AutoSize = true;
+            IDLabel.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            IDLabel.Location = new Point(217, 86);
+            IDLabel.Name = "IDLabel";
+            IDLabel.Size = new Size(108, 29);
+            IDLabel.TabIndex = 2;
+            IDLabel.Text = "User ID";
+            // 
+            // passLabel
+            // 
+            passLabel.AutoSize = true;
+            passLabel.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            passLabel.Location = new Point(217, 159);
+            passLabel.Name = "passLabel";
+            passLabel.Size = new Size(132, 29);
+            passLabel.TabIndex = 3;
+            passLabel.Text = "Password";
+            // 
+            // LoginBtn
+            // 
+            LoginBtn.CornerRoundingRadius = 20F;
+            LoginBtn.Location = new Point(266, 234);
+            LoginBtn.Name = "LoginBtn";
+            LoginBtn.Size = new Size(210, 59);
+            LoginBtn.StateCommon.Back.Color1 = Color.FromArgb(102, 66, 24);
+            LoginBtn.StateCommon.Back.Color2 = Color.FromArgb(102, 66, 24);
+            LoginBtn.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            LoginBtn.StateCommon.Border.Rounding = 20F;
+            LoginBtn.StateCommon.Content.LongText.Color1 = Color.White;
+            LoginBtn.StateCommon.Content.ShortText.Color1 = Color.White;
+            LoginBtn.StateCommon.Content.ShortText.Color2 = Color.White;
+            LoginBtn.StateCommon.Content.ShortText.Font = new Font("Gill Sans MT", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            LoginBtn.TabIndex = 4;
+            LoginBtn.Values.Text = "Log In";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(29, 60);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(162, 157);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(607, 5);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(63, 63);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
             // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(637, 450);
+            BackColor = Color.White;
+            ClientSize = new Size(682, 342);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(LoginBtn);
+            Controls.Add(passLabel);
+            Controls.Add(IDLabel);
+            Controls.Add(passTxtbx);
+            Controls.Add(idTxtBx);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "LoginForm";
             Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Krypton.Toolkit.KryptonTextBox idTxtBx;
+        private Krypton.Toolkit.KryptonTextBox passTxtbx;
+        private Label IDLabel;
+        private Label passLabel;
+        private Krypton.Toolkit.KryptonButton LoginBtn;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
