d18a08d807ec671992d85a77d4762fb8b52f1307
Omar Khattab <omar.khattab7@icloud.com>
2024-04-20 17:45:16 +0200

    Implement Admin Form UI

--- GUI/AdminForm.Designer.cs
+++ GUI/AdminForm.Designer.cs
@@ -28,12 +28,438 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminForm";
+            sailedItemsLabel = new Label();
+            label1 = new Label();
+            sailedCoffee = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            label20 = new Label();
+            label21 = new Label();
+            label22 = new Label();
+            label23 = new Label();
+            label24 = new Label();
+            label25 = new Label();
+            label26 = new Label();
+            label27 = new Label();
+            label28 = new Label();
+            label29 = new Label();
+            label30 = new Label();
+            SuspendLayout();
+            // 
+            // sailedItemsLabel
+            // 
+            sailedItemsLabel.AutoSize = true;
+            sailedItemsLabel.Font = new Font("Rockwell", 18F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            sailedItemsLabel.Location = new Point(33, 21);
+            sailedItemsLabel.Name = "sailedItemsLabel";
+            sailedItemsLabel.Size = new Size(263, 44);
+            sailedItemsLabel.TabIndex = 0;
+            sailedItemsLabel.Text = "Sailed Items :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Rockwell", 16F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.Location = new Point(33, 259);
+            label1.Name = "label1";
+            label1.Size = new Size(187, 39);
+            label1.TabIndex = 1;
+            label1.Text = "Inventory :";
+            // 
+            // sailedCoffee
+            // 
+            sailedCoffee.AutoSize = true;
+            sailedCoffee.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            sailedCoffee.Location = new Point(178, 94);
+            sailedCoffee.Name = "sailedCoffee";
+            sailedCoffee.Size = new Size(90, 31);
+            sailedCoffee.TabIndex = 2;
+            sailedCoffee.Text = "Coffee";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(333, 330);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 31);
+            label2.TabIndex = 3;
+            label2.Text = "Catch";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(333, 385);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 31);
+            label3.TabIndex = 4;
+            label3.Text = "Catch";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Schoolbook", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(422, 562);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 33);
+            label4.TabIndex = 5;
+            label4.Text = "ml";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Schoolbook", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(422, 504);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 33);
+            label5.TabIndex = 6;
+            label5.Text = "gm";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century Schoolbook", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(422, 437);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 33);
+            label6.TabIndex = 7;
+            label6.Text = "ml";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Century Schoolbook", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(422, 382);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 33);
+            label7.TabIndex = 8;
+            label7.Text = "ml";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Century Schoolbook", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(422, 327);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 33);
+            label8.TabIndex = 9;
+            label8.Text = "gm";
+            label8.Click += label8_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Century Schoolbook", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(503, 192);
+            label9.Name = "label9";
+            label9.Size = new Size(71, 33);
+            label9.TabIndex = 10;
+            label9.Text = "Also";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Century Schoolbook", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(503, 140);
+            label10.Name = "label10";
+            label10.Size = new Size(71, 33);
+            label10.TabIndex = 11;
+            label10.Text = "Also";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Book Antiqua", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.Location = new Point(503, 94);
+            label11.Name = "label11";
+            label11.Size = new Size(71, 35);
+            label11.TabIndex = 12;
+            label11.Text = "9999";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.Location = new Point(333, 440);
+            label12.Name = "label12";
+            label12.Size = new Size(83, 31);
+            label12.TabIndex = 13;
+            label12.Text = "Catch";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label13.Location = new Point(333, 507);
+            label13.Name = "label13";
+            label13.Size = new Size(83, 31);
+            label13.TabIndex = 14;
+            label13.Text = "Catch";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label14.Location = new Point(333, 565);
+            label14.Name = "label14";
+            label14.Size = new Size(83, 31);
+            label14.TabIndex = 15;
+            label14.Text = "Catch";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label15.Location = new Point(333, 195);
+            label15.Name = "label15";
+            label15.Size = new Size(83, 31);
+            label15.TabIndex = 16;
+            label15.Text = "Catch";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label16.Location = new Point(333, 143);
+            label16.Name = "label16";
+            label16.Size = new Size(83, 31);
+            label16.TabIndex = 17;
+            label16.Text = "Catch";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label17.Location = new Point(333, 94);
+            label17.Name = "label17";
+            label17.Size = new Size(83, 31);
+            label17.TabIndex = 18;
+            label17.Text = "Catch";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label18.Location = new Point(178, 565);
+            label18.Name = "label18";
+            label18.Size = new Size(96, 31);
+            label18.TabIndex = 19;
+            label18.Text = "Vanilla";
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label19.Location = new Point(178, 507);
+            label19.Name = "label19";
+            label19.Size = new Size(81, 31);
+            label19.TabIndex = 20;
+            label19.Text = "Sugar";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label20.Location = new Point(178, 441);
+            label20.Name = "label20";
+            label20.Size = new Size(84, 31);
+            label20.TabIndex = 21;
+            label20.Text = "Water";
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label21.Location = new Point(178, 385);
+            label21.Name = "label21";
+            label21.Size = new Size(70, 31);
+            label21.TabIndex = 22;
+            label21.Text = "Milk";
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label22.Location = new Point(178, 330);
+            label22.Name = "label22";
+            label22.Size = new Size(90, 31);
+            label22.TabIndex = 23;
+            label22.Text = "Coffee";
+            // 
+            // label23
+            // 
+            label23.AutoSize = true;
+            label23.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label23.Location = new Point(178, 195);
+            label23.Name = "label23";
+            label23.Size = new Size(90, 31);
+            label23.TabIndex = 24;
+            label23.Text = "Coffee";
+            // 
+            // label24
+            // 
+            label24.AutoSize = true;
+            label24.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label24.Location = new Point(178, 143);
+            label24.Name = "label24";
+            label24.Size = new Size(90, 31);
+            label24.TabIndex = 25;
+            label24.Text = "Coffee";
+            // 
+            // label25
+            // 
+            label25.AutoSize = true;
+            label25.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label25.Location = new Point(580, 94);
+            label25.Name = "label25";
+            label25.Size = new Size(27, 31);
+            label25.TabIndex = 26;
+            label25.Text = "$";
+            // 
+            // label26
+            // 
+            label26.AutoSize = true;
+            label26.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label26.Location = new Point(580, 192);
+            label26.Name = "label26";
+            label26.Size = new Size(27, 31);
+            label26.TabIndex = 27;
+            label26.Text = "$";
+            // 
+            // label27
+            // 
+            label27.AutoSize = true;
+            label27.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label27.Location = new Point(580, 140);
+            label27.Name = "label27";
+            label27.Size = new Size(27, 31);
+            label27.TabIndex = 28;
+            label27.Text = "$";
+            // 
+            // label28
+            // 
+            label28.AutoSize = true;
+            label28.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label28.Location = new Point(178, 617);
+            label28.Name = "label28";
+            label28.Size = new Size(113, 31);
+            label28.TabIndex = 31;
+            label28.Text = "Caramel";
+            // 
+            // label29
+            // 
+            label29.AutoSize = true;
+            label29.Font = new Font("Garamond", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label29.Location = new Point(333, 617);
+            label29.Name = "label29";
+            label29.Size = new Size(83, 31);
+            label29.TabIndex = 30;
+            label29.Text = "Catch";
+            // 
+            // label30
+            // 
+            label30.AutoSize = true;
+            label30.Font = new Font("Century Schoolbook", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label30.Location = new Point(422, 614);
+            label30.Name = "label30";
+            label30.Size = new Size(49, 33);
+            label30.TabIndex = 29;
+            label30.Text = "ml";
+            // 
+            // AdminForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 678);
+            Controls.Add(label28);
+            Controls.Add(label29);
+            Controls.Add(label30);
+            Controls.Add(label27);
+            Controls.Add(label26);
+            Controls.Add(label25);
+            Controls.Add(label24);
+            Controls.Add(label23);
+            Controls.Add(label22);
+            Controls.Add(label21);
+            Controls.Add(label20);
+            Controls.Add(label19);
+            Controls.Add(label18);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(sailedCoffee);
+            Controls.Add(label1);
+            Controls.Add(sailedItemsLabel);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "AdminForm";
+            Text = "Catch here";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label sailedItemsLabel;
+        private Label label1;
+        private Label sailedCoffee;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private Label label20;
+        private Label label21;
+        private Label label22;
+        private Label label23;
+        private Label label24;
+        private Label label25;
+        private Label label26;
+        private Label label27;
+        private Label label28;
+        private Label label29;
+        private Label label30;
     }
 }
--- GUI/AdminForm.cs
+++ GUI/AdminForm.cs
@@ -16,5 +16,10 @@ namespace CoffeeMachineSystem.GUI
         {
             InitializeComponent();
         }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
