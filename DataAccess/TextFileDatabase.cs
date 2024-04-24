e17f629b0bc20fc19945e1201ccc01b3ca0f8a3e
OmarElprolosy66 <omarelsayed6600@gmail.com>
2024-04-25 01:54:51 +0200

    added the `TextFileDatabase.cs` that allows us to deal with text file database.Added the core logic in the user form  like updateing the database with evrey order and vallidation mechanism

---
+++ DataAccess/TextFileDatabase.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CoffeeMachineSystem.DataAccess
+{
+    internal class TextFileDatabase
+    {
+        private readonly string filePath;
+
+        /// <summary>
+        /// Initializes a new instance of the TextFileDatabase class with the specified file path.
+        /// </summary>
+        /// <param name="filePath">The path to the text file used as the database.</param>
+        public TextFileDatabase(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        /// <summary>
+        /// Inserts a new record into the text file database.
+        /// </summary>
+        /// <param name="record">The record to insert into the database.</param>
+        public void Insert(string record)
+        {
+            using FileStream fs = new FileStream(filePath, FileMode.Append, FileAccess.Write);
+            using StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(record);
+        }
+
+        /// <summary>
+        /// Reads all records from the text file database.
+        /// </summary>
+        /// <returns>A list containing all records read from the database.</returns>
+        public List<string> Read()
+        {
+            List<string> records = new List<string>();
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                while (!sr.EndOfStream)
+                {
+                    records.Add(sr.ReadLine());
+                }
+            }
+
+            return records;
+        }
+
+        /// <summary>
+        /// Reads the specified field of a record from the text file database based on the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the record to read.</param>
+        /// <param name="fieldIndex">The index of the field to read within the record.</param>
+        /// <param name="separator">The character used to separate fields in the records.</param>
+        /// <returns>The value of the specified field.</returns>
+        public string ReadFieldById(int id, int fieldIndex, char separator)
+        {
+            using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using StreamReader sr = new StreamReader(fs);
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] fields = line.Split(separator);
+                if (fields.Length > fieldIndex && int.Parse(fields[0]) == id)
+                {
+                    return fields[fieldIndex];
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Deletes a record from the text file database based on the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the record to delete.</param>
+        /// <param name="separator">The character used to separate fields in the records.</param>
+        public void Delete(int id, char separator)
+        {
+            List<string> records = Read();
+
+            records.RemoveAll(r => int.Parse(r.Split(separator)[0]) == id);
+
+            WriteAllRecords(records);
+        }
+
+        /// <summary>
+        /// Updates a specific field of a record in the text file database based on the specified ID and field index.
+        /// </summary>
+        /// <param name="id">The ID of the record to update.</param>
+        /// <param name="fieldIndex">The index of the field to update within the record.</param>
+        /// <param name="newValue">The new value to set for the field.</param>
+        /// <param name="separator">The character used to separate fields in the records.</param>
+        public void UpdateField(int id, int fieldIndex, string newValue, char separator)
+        {
+            List<string> records = Read();
+
+            for (int i = 0; i < records.Count; i++)
+            {
+                string[] fields = records[i].Split(separator);
+
+                if (fieldIndex < fields.Length && int.Parse(fields[0]) == id)
+                {
+                    fields[fieldIndex] = newValue;
+                    records[i] = string.Join(separator, fields);
+                    break;
+                }
+            }
+
+            WriteAllRecords(records);
+        }
+
+        /// <summary>
+        /// Writes all records to the text file database.
+        /// </summary>
+        /// <param name="records">The list of records to write to the database.</param>
+        private void WriteAllRecords(List<string> records)
+        {
+            using FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            using StreamWriter writer = new StreamWriter(fs);
+
+            foreach (string record in records)
+            {
+                writer.WriteLine(record);
+            }
+        }
+    }
+}
--- GUI/UserForm.cs
+++ GUI/UserForm.cs
@@ -10,100 +10,75 @@ using System.Windows.Forms;
 
 namespace CoffeeMachineSystem.GUI
 {
+    internal enum EnSizes
+    {
+        small = 0,
+        medium = 1,
+        large = 2,
+    }
+
+    internal enum EnDrinks
+    {
+        coffee = 0,
+        cappuccino = 1,
+        latte = 2,
+    }
+
+    internal struct StIngredients
+    {
+        public short water;
+        public short milk;
+        public short coffee;
+        public short sugar;
+        public short vanilla;
+        public short caramel;
+    }
+
     public partial class UserForm : Form
     {
+        private EnDrinks drinkName = EnDrinks.coffee;
+        private EnSizes size = EnSizes.small;
+        private short quantity;
+        private StIngredients ingred;
+        private StIngredients dbIngred;
+        private DataAccess.TextFileDatabase fdb = new("../../../Resourses/Data.txt");
+
         public UserForm()
         {
             InitializeComponent();
         }
 
-
         /*---------------------------------------------------------------------------*/
-        //This 6 buttom for coffe type and size
+        //This 6 buttom for coffee type and size
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // This is a coffe button
+        // Button click events for selecting the drink type
+        private void button1_Click(object sender, EventArgs e) => drinkName = EnDrinks.coffee;
+        private void button2_Click(object sender, EventArgs e) => drinkName = EnDrinks.cappuccino;
+        private void button3_Click(object sender, EventArgs e) => drinkName = EnDrinks.latte;
 
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            // This is a cappuccino button 
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            // This is a latte` button 
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            // This is a small size botton 
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            // This is a medium size botton 
-
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            // This is a large size botton 
-
-        }
+        // Button click events for selecting the drink size
+        private void button4_Click(object sender, EventArgs e) => size = EnSizes.small;
+        private void button6_Click(object sender, EventArgs e) => size = EnSizes.medium;
+        private void button5_Click(object sender, EventArgs e) => size = EnSizes.large;
 
         /*---------------------------------------------------------------------------*/
         //This 4 buttom for Suger and quantity
 
+        // Button click events for adjusting sugar quantity
+        private void button7_Click(object sender, EventArgs e) {/* Implement decrease sugar quantity */}
+        private void button8_Click(object sender, EventArgs e) {/* Implement increase sugar quantity */}
 
-        //For Suger
-        private void button7_Click(object sender, EventArgs e)
-        {
-            // This is a buttom for minus suger 
-
-        }
-        private void button8_Click(object sender, EventArgs e)
-        {
-            // This is a buttom for plus suger 
-
-        }
-
-
-        //For Quantity
-        private void button9_Click(object sender, EventArgs e)
-        {
-            // This is a buttom for minus quantity 
-
-        }
-        private void button10_Click(object sender, EventArgs e)
-        {
-            // This is a buttom for plus quantity 
-
-        }
+        // Button click events for adjusting drink quantity
+        private void button9_Click(object sender, EventArgs e) {/* Implement decrease drink quantity */}
+        private void button10_Click(object sender, EventArgs e) {/* Implement increase drink quantity */}
 
         /*---------------------------------------------------------------------------*/
 
-        //For Syrups
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-
-
-        {
-            //Vanilla buttom
-        }
-
-
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-            //Caramel buttom
-        }
+        // Checkbox events for selecting vanilla or caramel syrup
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {/* Implement selection of vanilla syrup */}
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) {/* Implement selection of caramel syrup */}
 
         /*---------------------------------------------------------------------------*/
-
-
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
@@ -128,7 +103,128 @@ namespace CoffeeMachineSystem.GUI
         //Click buttom
         private void button11_Click(object sender, EventArgs e)
         {
+            // Get the quantity of drinks
+            quantity = short.Parse(textBox4.Text);
 
+            // Read the available ingredients from the database
+            dbIngred.water   = short.Parse(fdb.ReadFieldById(1, 2, ','));
+            dbIngred.milk    = short.Parse(fdb.ReadFieldById(2, 2, ','));
+            dbIngred.coffee  = short.Parse(fdb.ReadFieldById(3, 2, ','));
+            dbIngred.sugar   = short.Parse(fdb.ReadFieldById(4, 2, ','));
+            dbIngred.vanilla = short.Parse(fdb.ReadFieldById(5, 2, ','));
+            dbIngred.caramel = short.Parse(fdb.ReadFieldById(6, 2, ','));
+
+            // Store the required ingredients' data based on drink type and size
+            switch (drinkName)
+            {
+                case (EnDrinks.coffee):
+                    switch (size)
+                    {
+                        case (EnSizes.large):
+                            ingred.water = 470;
+                            ingred.milk = 60;
+                            ingred.coffee = 30;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                        case (EnSizes.medium):
+                            ingred.water = 225;
+                            ingred.milk = 30;
+                            ingred.coffee = 15;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                        case (EnSizes.small):
+                            ingred.water = 125;
+                            ingred.milk = 20;
+                            ingred.coffee = 10;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                    }
+                    break;
+                case (EnDrinks.cappuccino):
+                    switch (size)
+                    {
+                        case (EnSizes.large):
+                            ingred.water = 470;
+                            ingred.milk = 60;
+                            ingred.coffee = 30;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                        case (EnSizes.medium):
+                            ingred.water = 225;
+                            ingred.milk = 30;
+                            ingred.coffee = 15;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                        case (EnSizes.small):
+                            ingred.water = 125;
+                            ingred.milk = 20;
+                            ingred.coffee = 10;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                    }
+                    break;
+                case (EnDrinks.latte):
+                    switch (size)
+                    {
+                        case (EnSizes.large):
+                            ingred.water = 470;
+                            ingred.milk = 60;
+                            ingred.coffee = 30;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                        case (EnSizes.medium):
+                            ingred.water = 225;
+                            ingred.milk = 30;
+                            ingred.coffee = 15;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                        case (EnSizes.small):
+                            ingred.water = 125;
+                            ingred.milk = 20;
+                            ingred.coffee = 10;
+                            ingred.sugar = short.Parse(textBox2.Text);
+                            break;
+                    }
+                    break;
+            }
+
+            // Check if there are enough basic ingredients
+            if (ingred.water  * quantity > dbIngred.water  ||
+                ingred.milk   * quantity > dbIngred.milk   ||
+                ingred.coffee * quantity > dbIngred.coffee ||
+                ingred.sugar  * quantity > dbIngred.sugar)
+            {
+                MessageBox.Show("There are not enough ingredients!");
+                return;
+            }
+            else
+            {
+                // Check if there are enough flavor ingredients (vanilla or caramel)
+                if (checkBox1.Checked && dbIngred.vanilla < 30 * quantity)
+                {
+                    MessageBox.Show("There are not enough vanilla!");
+                    return;
+                }
+                if (checkBox2.Checked && dbIngred.caramel < 30 * quantity)
+                {
+                    MessageBox.Show("There are not enough caramel!");
+                    return;
+                }
+
+                // Update the database with the used ingredients
+                fdb.UpdateField(1, 2, (dbIngred.water - ingred.water * quantity).ToString(), ',');
+                fdb.UpdateField(2, 2, (dbIngred.milk - ingred.milk * quantity).ToString(), ',');
+                fdb.UpdateField(3, 2, (dbIngred.coffee - ingred.coffee * quantity).ToString(), ',');
+                fdb.UpdateField(4, 2, (dbIngred.sugar - ingred.sugar * quantity).ToString(), ',');
+
+                // Update the database with the used flavor ingredients (vanilla or caramel)
+                if (checkBox1.Checked)
+                    fdb.UpdateField(5, 2, (dbIngred.vanilla - 30 * quantity).ToString(), ',');
+                if (checkBox2.Checked)
+                    fdb.UpdateField(6, 2, (dbIngred.caramel - 30 * quantity).ToString(), ',');
+
+                MessageBox.Show("Your order has been placed successfully!");
+            }
         }
     }
 }
